01e658eda8e01fe2c0f23e35775d4db6b8193807
boburshoh01 <temirovbobur8@gmail.com>
2023-10-23 12:37:43 +0500

    news

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: false,
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
-
+};
