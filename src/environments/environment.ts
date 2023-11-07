a9bcf5f659d3b356823c3059d11cb668219f44a3
boburshoh01 <temirovbobur8@gmail.com>
2023-11-07 15:59:50 +0500

    news

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA1UXUVIobHL6PAG5LZKxn6H8vMnGUVfhE",
+    authDomain: "clips-ae9e0.firebaseapp.com",
+    projectId: "clips-ae9e0",
+    storageBucket: "clips-ae9e0.appspot.com",
+
+    appId: "1:82326730177:web:bbdbbc826513749c8feb40"
+  }
+};
