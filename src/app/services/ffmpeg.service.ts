d1b204e85ef5ecd72e3a93663df968fec4fbc671
boburshoh01 <temirovbobur8@gmail.com>
2023-12-05 18:06:53 +0500

    news

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import { FFmpeg } from '@ffmpeg/ffmpeg'
-import { fetchFile } from '@ffmpeg/util';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FfmpegService {
+  isRunning = false
   isReady = false
   private ffmpeg
 
   constructor() {
-    this.ffmpeg = new FFmpeg()
+    this.ffmpeg = createFFmpeg({ log: true })
   }
 
   async init() {
@@ -19,12 +19,63 @@ export class FfmpegService {
     }
 
     await this.ffmpeg.load()
+
     this.isReady = true
   }
 
   async getScreenshots(file: File) {
+    this.isRunning = true
+
     const data = await fetchFile(file)
 
-    this.ffmpeg.writeFile(file.name, data)
+    this.ffmpeg.FS('writeFile', file.name, data)
+
+    const seconds = [1, 2, 3]
+    const commands: string[] = []
+
+    seconds.forEach(second => [
+      commands.push(
+        // Input
+        '-i', file.name,
+        // Output Options
+        '-ss', `00:00:0${second}`,
+        '-frames:v', '1',
+        '-filter:v', 'scale=510:-1',
+        //Output
+        `output_0${second}.png`
+      )
+    ])
+
+    await this.ffmpeg.run(
+      ...commands
+    )
+
+    const screenshots: string[] = []
+
+    seconds.forEach(second => {
+      const screenshotFile = this.ffmpeg.FS(
+        'readFile', `output_0${second}.png`
+      )
+      const screenshotBlob = new Blob(
+        [screenshotFile.buffer], {
+        type: 'image/png'
+      }
+      )
+
+      const screenshotUrl = URL.createObjectURL(screenshotBlob)
+
+      screenshots.push(screenshotUrl)
+    })
+
+    this.isRunning = false
+
+    return screenshots
+  }
+
+  async blobFromURL(url: string) {
+    const response = await fetch(url)
+    const blob = await response.blob()
+
+    return blob
   }
 }
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { last, switchMap } from 'rxjs';
+import { combineLatest, forkJoin, switchMap } from 'rxjs';
 import { v4 as uuid } from 'uuid';
 import firebase from 'firebase/compat/app'
 import { ClipService } from 'src/app/services/clip.service';
@@ -26,6 +26,9 @@ export class UploadComponent implements OnDestroy {
   showPercentage = false
   user: firebase.User | null = null
   task?: AngularFireUploadTask
+  screenshots: string[] = []
+  selectedScreenshot = ''
+  screenshotTask?: AngularFireUploadTask
 
   title = new FormControl('', {
     validators: [
@@ -55,6 +58,10 @@ export class UploadComponent implements OnDestroy {
   }
 
   async storeFile($event: Event) {
+    if (this.ffmpegService.isRunning) {
+      return
+    }
+
     this.isDragover = false
 
     this.file = ($event as DragEvent).dataTransfer ?
@@ -64,13 +71,15 @@ export class UploadComponent implements OnDestroy {
       return
     }
 
-    this.ffmpegService.getScreenshots(this.file)
+    this.screenshots = await this.ffmpegService.getScreenshots(this.file)
+
+    this.selectedScreenshot = this.screenshots[0]
 
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''))
     this.nextStep = true
   }
 
-  uploadFile() {
+  async uploadFile() {
     this.uploadForm.disable()
 
     this.showAlert = true
@@ -82,30 +91,54 @@ export class UploadComponent implements OnDestroy {
     const clipFileName = uuid()
     const clipPath = `clips/${clipFileName}.mp4`
 
+    const screenshotBlob = await this.ffmpegService.blobFromURL(this.selectedScreenshot)
+    const screenshotPath = `screenshots/${clipFileName}.png`
+
     this.task = this.storage.upload(clipPath, this.file)
     const clipRef = this.storage.ref(clipPath)
 
-    this.task.percentageChanges().subscribe(progress => {
-      this.percentage = progress as number / 100
+    this.screenshotTask = this.storage.upload(screenshotPath, screenshotBlob)
+    const screenshotRef = this.storage.ref(screenshotPath)
+
+    combineLatest([
+      this.task.percentageChanges(),
+      this.screenshotTask.percentageChanges()
+    ]).subscribe((progress) => {
+      const [clipProgress, screenshotProgress] = progress
+
+      if (!clipProgress || !screenshotProgress) {
+        return
+      }
+
+      const total = clipProgress + screenshotProgress
+
+      this.percentage = total as number / 200
     })
 
-    this.task.snapshotChanges().pipe(
-      last(),
-      switchMap(() => clipRef.getDownloadURL())
+    forkJoin([
+      this.task.snapshotChanges(),
+      this.screenshotTask.snapshotChanges()
+    ]).pipe(
+      switchMap(() => forkJoin([
+        clipRef.getDownloadURL(),
+        screenshotRef.getDownloadURL()
+      ]))
     ).subscribe({
-      next: async (url) => {
+      next: async (urls) => {
+        const [clipURL, screenshotURL] = urls
+
         const clip = {
           uid: this.user?.uid as string,
           displayName: this.user?.displayName as string,
           title: this.title.value,
           fileName: `${clipFileName}.mp4`,
-          url,
+          url: clipURL,
+          screenshotURL,
+          screenshotFileName: `${clipFileName}.png`,
           timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }
 
         const clipDocRef = await this.clipsService.createClip(clip)
-
-        console.log(clip);
 
         this.alertColor = 'green'
         this.alertMsg = 'Success! Your clip is now ready to share with the world.'
